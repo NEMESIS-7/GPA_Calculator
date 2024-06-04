5a519a94330792c0afa1dab2204defba2b947acb
MVRK0 <elikemfenuksu@gmail.com>
2024-06-04 00:30:36 +0000

    refactor: slight corrections to syntax

--- GPA_CALCULATOR.java
+++ GPA_CALCULATOR.java
@@ -5,36 +5,36 @@ public class GPA_CALCULATOR {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the number of courses you took: ");
-        int numberofcourses = input.nextInt();
+        int numberOfCourses = input.nextInt();
 
         //arrays to store grades and credit hours
-        double[] grades = new double[(int) numberofcourses];
-        int[] credhrs = new int[(int) numberofcourses];
+        double[] grades = new double[(int) numberOfCourses];
+        int[] creditHours = new int[(int) numberOfCourses];
 
         // storing the grade and credit hours for each course the user is taking, using a for loop
-        // doing this allows each grade to be stored to each single index in the array, the size of the arrays are determined by the user(numberofcourse and credhrs)
-        for (int i = 0; i < numberofcourses; i++){
-            System.out.print("enter the grade: ");
+        // doing this allows each grade to be stored to each single index in the array, the size of the arrays are determined by the user(numberOfCourses and creditHours)
+        for (int i = 0; i < numberOfCourses; i++){
+            System.out.print("Enter the grade: ");
             grades[i] = input.nextDouble();
-            System.out.print("enter the credit hours: ");
-            credhrs[i] = input.nextInt();
+            System.out.print("Enter the credit hours: ");
+            creditHours[i] = input.nextInt();
         }
         // initializing variables to store the total grade points and the total credit hours
-        double totalgradepnt = 0;
-        int totalcredhrs = 0;
+        double totalGradepoints = 0;
+        int totalCreditHours = 0;
 
         //for loop iterating through the grades stored in the grades array and the credit hours array
         //then multiplying the grades by the credit hours to get the total grade points
-        for (int i = 0; i < numberofcourses; i++){
-            totalgradepnt += grades[i];
-            totalcredhrs += credhrs[i];
+        for (int i = 0; i < numberOfCourses; i++){
+            totalGradepoints += grades[i];
+            totalCreditHours += creditHours[i];
         }
-        System.out.println("your total grade points are: " + totalgradepnt);
-        System.out.println("your total credit hours are: " + totalcredhrs);
+        System.out.println("Your total grade points are: " + totalGradepoints);
+        System.out.println("Your total credit hours are: " + totalCreditHours);
 
-        double GPA = totalgradepnt / totalcredhrs;
+        double GPA = totalGradepoints / totalCreditHours;
 
-        System.out.println("your GPA is: " + GPA);
+        System.out.println("Your GPA is: " + GPA);
 
         // if statements to assign classes to the GPA
         if ( GPA <= 4 && GPA >= 3.60 ){
